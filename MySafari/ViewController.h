ecc534ddd2a9c08f39f4a399b841811bedb82811
Martin Henry <emailnitram@gmail.com>
2015-10-22 18:12:24 -0700

    Initial Commit

---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Martin Henry on 10/22/15.
+//  Copyright © 2015 Martin Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MySafari/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MySafari
+//
+//  Created by Martin Henry on 10/22/15.
+//  Copyright © 2015 Martin Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
